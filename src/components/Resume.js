4b94c0abf34d3d5ed305da7c4a00d4774173c93f
AbaniwoM <michaelabaniwo@gmail.com>
2021-11-26 06:16:57 +0100

    built my resume

---
+++ src/components/Resume.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div className="container">
+            <div className="headerContainer">
+                <div className="header1">
+                    <div className="head1">Michael Abaniwo</div>
+                    <div className="head2">Frontend React Web Developer</div>
+                    <div className="para">
+                        <div className="para3">08076578993 | michaelabaniwo@gmail.com</div>
+                        <div className="para4">Amudalat Onigbanjo Crescent, United Estate, Sangotedo, Ibeju-Lekki, Lagos, Nigeria.</div>
+                    </div>
+                </div>
+            </div>
+            <div className="bodyWrapper">
+            <div className="summary">
+                <div className="sum">SUMMARY</div>
+                <div className="sumP">Detailed-oriented Software Engineer with 2 years of experience and a zest for solving complex problems in frontend web development. Seeking to use proven web development skills to improve quality, cost and time metrics for your company.</div>
+            </div>
+            <div className="github">
+                <div className="git">GITHUB</div>
+                <div className="hub">
+                    Click here: <a href="https://github.com/AbaniwoM">https://github.com/AbaniwoM</a>
+                </div>
+            </div>
+            <div className="portfolio">
+                <div className="port">MY PORTFOLIO</div>
+                <div className="list">
+                    Click here: <a href="https://michael-abaniwos-portfolio.netlify.app/">https://michael-abaniwos-portfolio.netlify.app/</a>
+                </div>
+            </div>
+            <div className="project">
+                <div className="proj">SOME PROJECTS DONE</div>
+                <div className="list2">
+                    <li>Click here: <a href="https://loanvest.netlify.app/">https://loanvest.netlify.app/</a></li>
+                    <li>Click here: <a href="https://vetconsultweb.netlify.app/">https://vetconsultweb.netlify.app/</a></li>
+                    <li>Click here: <a href="https://skyhigh-sales-data-app.netlify.app/">https://skyhigh-sales-data-app.netlify.app/</a></li>
+                    <li>Click here: <a href="https://abaniwom.github.io/mikhael-is-here/">https://abaniwom.github.io/mikhael-is-here/</a></li>
+                    <li>Click here: <a href="https://abaniwom.github.io/my-todo-list/">https://abaniwom.github.io/my-todo-list/</a></li>
+                    <li>Click here: <a href="https://abaniwom.github.io/voting-app-online-poll/">https://abaniwom.github.io/voting-app-online-poll/</a></li>
+                    <li>Click here: <a href="https://abaniwom.github.io/voting-app-online-results/">https://abaniwom.github.io/voting-app-online-results/</a></li>
+                    <li>Click here: <a href="https://sweet-pastery-app.netlify.app/">https://sweet-pastery-app.netlify.app/</a></li>
+                </div>
+            </div>
+            <div className="workExperience">
+                <div className="work">WORK EXPERIENCE</div>
+                <div className="zuri">
+                <div className="zuriHeader">
+                    <div className="zurih">03/2021 - Present</div>
+                    <div className="zurih2">Intern at Zuri.team</div>
+                    <div className="zurih3"><a href="https://zuri.team">https://zuri.team</a></div>
+                </div>
+                <div className="workP">- Developed scalable Projects using JavaScript programming languages, HTML5 and CSS Web Technologies.</div>
+                <div className="workP2">- Part of the team building a Veterinary Consultation web application to bridge the gap between pet owners and veterinary doctors.</div>
+                </div>
+                <div className="truparse">
+                    <div className="truHeader">
+                        <div className="tru1">04/2020 - 5/2020</div>
+                        <div className="tru2">Junior Frontend Web Developer at Truparse Nigeria Limited</div>
+                    </div>
+                    <div className="truP">- Implemented APIs and built UI Web designs using HTML5, CSS and JavaScript.</div>
+                    <div className="truP2">- Joined team to research on API fetching and implementation.</div>
+                </div>
+            </div>
+            <div className="education">
+                <div className="edu">EDUCATION</div>
+                <div className="eduHeader">
+                    <div className="edu2">09/2016 - 09/2019</div>
+                    <div className="edu3">University of Lagos, Nigeria. Akoka, Yaba</div>
+                    <div className="edu4">BSc. Marine Biology</div>
+                </div>
+            </div>
+            <div className="certifications">
+                <div className="cert">CERTIFICATIONS</div>
+                <div className="certP"><li>Certificate of successful training with Udemy on real-life frontend programming.</li></div>
+            </div>
+            <div className="skills">
+                <div className="ski1">SKILLS</div>
+                <div className="ski2">Programming languages/Technologies and Proficiency on a scale of 1 to 10 (1 is lowest and 10 is highest):</div>
+                <div className="skiP">
+                    <li>HTML5 (10)</li> 
+                    <li>CSS (10)</li> 
+                    <li>JavaScript (8)</li>
+                    <li>React.js (10)</li>
+                    <li>Bootstrap (8)</li>
+                    <li>Node.js (8)</li>
+                    <li>Next.js (8)</li>
+                    <li>TailwindCss (8)</li>
+                </div>
+                <div className="ski3">Web Technologies:</div>
+                <div className="skiP2"><li>TailwindCss, GraphQL</li></div>
+                <div className="ski4">Tools:</div>
+                <div className="skiP3"><li>VsCode, Firebase, Zoom, Microsoft Teams, Google Meet, Figma, Jitsi</li></div>
+            </div>
+            <div className="footer">
+               <h3>Designed and implemented by <a href="https://www.linkedin.com/in/michael-abaniwo-521620193/">Michael Abaniwo</a></h3>
+               <h4>&copy; 2021</h4>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Resume;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Resume from './components/Resume';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Resume />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
